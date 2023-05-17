f12ab38d2d32aa148a645eabafa2d4ddf3c2a0cf
Guilherme Costa <guilherme.partic@gmail.com>
2023-05-17 15:17:35 -0300

    style: created a modal images

--- base_static/global/js/script.js
+++ base_static/global/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-// modal
-let imgModal = document.querySelector('#img-modal');
+// modal before open
+let imgModal = document.querySelector('#C-modal_defaul_img');
 const images = document.querySelectorAll('.C-detail_gallery_img');
 
 images.forEach((img) => {
@@ -10,3 +10,52 @@ images.forEach((img) => {
         imgModal.setAttribute('src', path);
     })
 })
+
+
+// modal after open
+const defaultImg = document.querySelector('#C-modal_defaul_img');
+const defaulContainerImg = document.querySelector('.C-detail_c_img');
+const modal = document.querySelector('.C-modal');
+const modalImg = document.querySelector('.C-modal_modal-img');
+const allImages = document.querySelectorAll('.C-detail_gallery_img');
+const buttonRight = document.querySelector('#C-modal_button-right');
+const buttonLeft = document.querySelector('#C-modal_button-left');
+const buttonCloseModal = document.querySelector('#C-modal_close');
+
+(() => {
+    const imagesLenght = allImages.length;
+    let count = 0;
+
+    defaulContainerImg.addEventListener("click", () => {
+        const path = defaultImg.getAttribute('src');
+        modalImg.setAttribute('src', path);
+        modal.classList.toggle('is_disabled');
+        modal.classList.toggle('modal-open');
+    })
+
+    buttonCloseModal.addEventListener("click", () => {
+        modal.classList.toggle('is_disabled');
+        modal.classList.toggle('modal-open');
+    })
+    
+    buttonRight.addEventListener("click", () => {
+        if (count < imagesLenght) {
+            try {
+                let path = allImages[count + 1].getAttribute('src');
+                count++;
+                modalImg.setAttribute('src', path);
+            } catch(e) {e};
+        }
+    
+    })
+    
+    buttonLeft.addEventListener("click", () => {
+        if (count > 0 ) {
+            try {
+                let path = allImages[count-1].getAttribute('src');
+                count--;
+                modalImg.setAttribute('src', path);
+            } catch(e) {e};
+        }
+    })
+})()
