b29f941146d430328c03215c9284a76839522485
Guilherme Costa <guilherme.partic@gmail.com>
2023-05-18 17:30:46 -0300

    fix: removed load_dotenv()

--- project/settings/environement.py
+++ project/settings/environement.py
@@ -12,10 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from utils.string.format_string import format_string_into_list
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
--- project/settings/security.py
+++ project/settings/security.py
@@ -1,7 +1,5 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
