f1e8ba016923fe57ff9ec4d37ed7a2a877e7a2b0
Guilherme Costa <guilherme.partic@gmail.com>
2023-05-19 00:40:37 -0300

    add load_dotenv

--- project/settings/databases.py
+++ project/settings/databases.py
@@ -1,15 +1,19 @@
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+from dotenv import load_dotenv
 import os
+
+
+load_dotenv()
 
 
 DATABASES = {
     'default': {
-        'ENGINE': str(os.environ.get('DATABASE_ENGINE')),
-        'NAME': str(os.environ.get('DATABASE_NAME')),
-        'USER': str(os.environ.get('DATABASE_USER')),
-        'PASSWORD': str(os.environ.get('DATABASE_PASSWORD')),
-        'HOST': str(os.environ.get('DATABASE_HOST')),
-        'PORT': str(os.environ.get('DATABASE_PORT')),
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
