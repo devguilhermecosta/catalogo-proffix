774e48f1e0d4e38539da83def24f69ade91c6d76
Guilherme Costa <guilherme.partic@gmail.com>
2023-05-19 00:30:40 -0300

    removed csrf

--- project/settings/environement.py
+++ project/settings/environement.py
@@ -51,8 +51,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-CSRF_TRUSTED_ORIGINS = [
-    os.environ.get('CSRF_TRUSTED_ORIGINS')
-]
