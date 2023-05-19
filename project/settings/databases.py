212eea6a8048cd521e2c42dc2da1563e3c93126d
Guilherme Costa <guilherme.partic@gmail.com>
2023-05-18 23:23:45 -0300

    fix: changed confis

--- project/settings/databases.py
+++ project/settings/databases.py
@@ -1,19 +1,15 @@
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-from dotenv import load_dotenv
 import os
-
-
-load_dotenv()
 
 
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DATABASE_ENGINE'),
         'NAME': os.environ.get('DATABASE_NAME'),
-        'DATABASE_USER': os.environ.get('DATABASE_USER'),
-        'DATABASE_PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'DATABASE_HOST': os.environ.get('DATABASE_HOST'),
-        'DATABASE_PORT': os.environ.get('DATABASE_PORT'),
-        }
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT':  os.environ.get('DATABASE_PORT'),
+    }
 }
