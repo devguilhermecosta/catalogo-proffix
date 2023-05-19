1f48022d6ccbced925fadd7141e5daea00468c82
Guilherme Costa <guilherme.partic@gmail.com>
2023-05-19 09:06:46 -0300

    modified the import in daabases

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -1,8 +1,8 @@
 # flake8: noqa
-from .environement import *
-from .installed_apps import *
-from .middleware import *
-from .templates import *
-from .databases import *
-from .timezone import *
-from .security import *
+from . environement import *
+from . installed_apps import *
+from . middleware import *
+from . templates import *
+from . databases import *
+from . timezone import *
+from . security import *
